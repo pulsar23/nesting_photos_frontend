1fcef93e82ae42a4e5d14153e20029c3cd8f24ad
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-10-24 19:51:32 -0500

    added photo views

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import  Home  from './components/Home.js'
 import  Login  from './components/Login.js'
 import  SignUp  from './components/SignUp.js'
 import  UserHome from './components/UserHome.js'
-import ReactDOM from 'react-dom';
+import  Album from './components/Album.js'
 import { Route, Switch, Redirect, NavLink, withRouter } from 'react-router-dom';
 
 class App extends Component {
@@ -14,8 +13,13 @@ class App extends Component {
     username: "",
     password: "",
     email: "",
+    albums: [],
+    title: "",
+    isLoadedAlbums: false,
+    toggleUpdate: true,
     currentUser: {}
   }
+
   componentDidMount(){
     //Check for token.
     // 1. IF and only if there is a token, make an authenticated request to
@@ -27,7 +31,8 @@ class App extends Component {
     //console.log(this)
     console.log("In componentDidMount")
 
-    if (!!token && this.props.location.pathname === "/login"){
+    //if (!!token && this.props.location.pathname === "/login"){
+    if (!!token){
       fetch("http://localhost:3000/api/v1/users/profile", {
         headers: {
           "Content-Type": "application/json",
@@ -35,11 +40,14 @@ class App extends Component {
         }
       })
         .then(resp => resp.json())
-        .then(data => this.setState({currentUser: data}, ()=>{this.props.history.push(`/profile/${this.state.currentUser.id}`)}))
+        .then(data => this.setState({currentUser: data, albums: data.albums, isLoadedAlbums: true},
+          ()=>{if (this.props.location.pathname === '/login' || this.props.location.pathname === '/signup'){
+          this.props.history.push(`/profile/${this.state.currentUser.id}`)}}))
     }
   }//end componentDidMount
 
-  handleOnChange = event =>{
+  handleOnChange = (event) =>{
+
     this.setState({
       [event.target.name]: event.target.value
     })
@@ -61,15 +69,18 @@ class App extends Component {
     })
       .then(resp => resp.json())
       .then(data=>{
+        if (data.user){
         localStorage.token = data.token
         this.setState({
           currentUser: data.user
         }, ()=>{this.props.history.push(`/profile/${this.state.currentUser.id}`)})
+      }//if
+      else{console.log(data)}
       })
   }
 
   handleLogin = event =>{
-
+    console.log("In handleLogin")
     event.preventDefault()
     fetch("http://localhost:3000/api/v1/users/login", {
       method: "POST",
@@ -83,11 +94,13 @@ class App extends Component {
     })
       .then(resp => resp.json())
       .then(data => {
+        console.log(data);
         if (!data.error){
-          console.log("hello world")
           localStorage.token = data.token
+          debugger
           this.setState({
-            currentUser: data.user
+            currentUser: data.user,
+            isLoadedAlbums: true
           }, ()=>{this.props.history.push(`/profile/${this.state.currentUser.id}`)})
         }
         else{
@@ -98,18 +111,65 @@ class App extends Component {
       })
   }// end handleLogin
 
-  handleLogout = event =>{
-    console.log('handleLogout')
+  handleLogout = (userID) =>{
+    localStorage.clear()
+    this.setState({token: ""})
+    this.props.history.push(`/`)
   }
-//<Route path="/profile/:user_id" render={props=> <div> Hello World {props.match.params.user_id}</div>}/>
+
+  getToggleUpdate = () =>{
+    console.log("getToggleUpdate", this.state.toggleUpdate)
+    this.setState({toggleUpdate: !this.state.toggleUpdate})
+  }
+
+//*****************************************************
+//The following code below handles Creating a New Album
+//*****************************************************
+  //
+  // handleCreateAlbum = () =>{
+  //   debugger
+  //   console.log('handleCreateAlbum')
+  //   let token = localStorage.token;
+  //   let API_USER_ALBUMS = 'http://localhost:3000/api/v1/albums'
+  //   console.log("token = ", token, "API_USER_ALBUMS = ", API_USER_ALBUMS)
+  //   fetch(API_USER_ALBUMS, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Authorization": `Bearer ${token}`
+  //     },
+  //     body: JSON.stringify({
+  //       "title": `${this.state.title}`,
+  //       "admin_id": `${this.state.userID}`
+  //     })
+  //   })//end fetch
+  //   .then(resp => resp.json())
+  //   .then(data => console.log(data))
+  // }//end handleCreateAlbum
+  //
+  // handleAlbumSubmit = (event) =>{
+  //   debugger
+  //   event.preventDefault()
+  //   console.log("handleAlbumSubmit",this.state.title)
+  //   this.handleCreateAlbum()
+  // }
+  //*****************************************************
+
+
+
   render() {
+    // if (!this.state.isLoadedAlbums){
+    //   return (<h3>Loading...</h3>)
+    // }
+    console.log("App.js-render", this.state.currentUser)
     return (
       <div className="App">
       <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/login" render={props=> <Login handleLogin={this.handleLogin} handleOnChange={this.handleOnChange} loginError={this.state.loginError} {...props}/>} />
-      <Route path="/profile/:user_id" render={props=> <UserHome handleLogout={this.handleLogout} {...props}/>} />
+      <Route path="/profile/:user_id" render={props=> <UserHome handleLogout={this.handleLogout} isLoadedAlbums={this.state.isLoadedAlbums} getToggleUpdate={this.getToggleUpdate} user={this.state.currentUser} {...props}/>} />
       <Route path="/signup" render={props=> <SignUp handleSignUp={this.handleSignUp} handleOnChange={this.handleOnChange} {...props}/>} />
+      <Route path="/albums/:album_id" render={props=> <Album user={this.state.currentUser} isLoadedAlbums={this.state.isLoadedAlbums} {...props}/>} />
       </Switch>
       </div>
     );
--- src/components/UserHome.js
+++ src/components/UserHome.js
@@ -1,13 +1,123 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import CreateAlbum from './CreateAlbum.js';
+import Album from './Album.js';
 
-const UserHome = (props) =>{
-  debugger
+
+class UserHome extends Component{
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      userID: this.props.match.params.user_id,
+      albums: [],
+      title: "",
+      toggleUpdate: true
+    }
+  }//end constructor
+
+  // componentDidMount(){
+  //   this.getUserData();
+  // }
+
+
+  // getUserData = () =>{
+  //   //let API_USER_ALBUMS = 'http://localhost:3000/api/v1/users/' + `${this.state.userID}` +'/albums'
+  //   let API_USER_ALBUMS = 'http://localhost:3000/api/v1/albums'
+  //   console.log(API_USER_ALBUMS)
+  //   fetch(API_USER_ALBUMS,{
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Authorization": `Bearer ${this.state.token}`
+  //     }
+  //   })//end fetch
+  //     .then(resp => resp.json())
+  //     .then(albums => this.setState({albums}))
+  //     .then(albums => console.log(albums))
+  // }
+
+
+  handleCreateAlbum = () =>{
+    console.log('handleCreateAlbum')
+    let token = localStorage.token;
+    let API_USER_ALBUMS = 'http://localhost:3000/api/v1/albums'
+    console.log("token = ", token, "API_USER_ALBUMS = ", API_USER_ALBUMS)
+    fetch(API_USER_ALBUMS, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        "title": `${this.state.title}`,
+        "admin_id": `${this.state.userID}`
+      })
+    })//end fetch
+    .then(resp => resp.json())
+    .then(data => this.setState({toggleUpdate: !this.state.toggleUpdate}))
+  }//end handleCreateAlbum
+
+  handleAlbumSubmit = (event) =>{
+    debugger
+    event.preventDefault()
+    console.log("handleAlbumSubmit",this.state.title)
+    this.handleCreateAlbum()
+  }
+
+  handleOnChange = (event) =>{
+    console.log("handleOnChange",event)
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+   getAlbumInfo = (albums) => {
+     console.log("In getAlbumInfo")
+     console.log(albums)
+     return albums.map((album) =>{
+       return (
+         <tr>
+          <td><Link to={`/albums/${album.id}`} params={album.id, album.title}> {album.title} </Link></td>
+          <td>{album.admin_id}</td>
+          <td>{this.state.username}</td>
+        </tr>
+      )
+    })
+    }//end getAlbumInfo
+
+    handleOnClick = (event) =>{
+      debugger
+      console.log("handleOnClick", event.target.textContent, event.target.dataset.id)
+      //For handleLogout
+    }
+
+  render(){
+    console.log("UserHome render", this.props)
+    console.log("After getUserData")
   return(
     <div>
-      <h1> User {props.match.params.user_id} Home Page </h1>
-      <button>Logout</button>
+      <h1> Welcome {this.props.user.username} : {this.state.userID} Home Page </h1>
+      <button onClick={()=>this.props.handleLogout(this.state.userID)}>Logout</button>
+      <table>
+        <tbody>
+        <tr>
+          <th>
+          <CreateAlbum handleOnChange={this.handleOnChange} handleAlbumSubmit={this.handleAlbumSubmit}/>
+          </th>
+        </tr>
+        <tr>
+            <th><h3>Albums  </h3></th>
+            <th><h3>Album Admin</h3></th>
+            <th><h3>Shared Users</h3></th>
+        </tr>
+          {this.props.isLoadedAlbums
+            ? this.getAlbumInfo(this.props.user.own_albums)
+            : <h3>Loading...</h3>
+            }
+        </tbody>
+      </table>
     </div>)
-}
+ }//end render
+}//end class User
 
 export default UserHome;
